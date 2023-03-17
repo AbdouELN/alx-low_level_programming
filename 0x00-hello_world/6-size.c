3dc1293542850e7f436c3394480a31f2d90866e7
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-16 17:24:58 -0700

    print size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-/**main - Retry point
+/**
+ * main - Retry point
  *
- *Descripion: using sizof to print the size of various types
+ * Descripion: using sizof to print the size of various types
  *
- * Return Always 0 (Seccess)
+ * Return: Always 0 (Seccess)
 */
 int main(void)
 {
-	char i;
-	int b;
-	long c;
-	long long int d;
-	float e;
-
-	printf("size of a char: %lu byte(s)\n", sizeof(i));
-	printf("size of an int: %lu byte(s)\n", sizeof(b));
-	printf("size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("size of a float: %lu byte(s)\n", sizeof(e));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
