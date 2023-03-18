54e8f925fcc571d84646f1a7a657b4c9dde0aff0
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-18 06:44:36 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Seccess)
+ */
+int main(void)
+{
+	int h;
+
+	for (h = 0; h <= 10; h++)
+	{
+		putchar(h + '0');
+	}
+	for (h = 0; h <= 6; h++)
+	{
+		putchar(h + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
