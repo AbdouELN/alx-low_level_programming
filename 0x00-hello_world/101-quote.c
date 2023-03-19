876c654e81db558424952f5bf0c6f9176eac09ce
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-19 02:56:46 -0700

    you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 1 (Seccess)
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\"- Dora Korpar, 2015-10-19");
-	putchar('\n');
+	char str[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
 	return (1);
 }
