aea3a4fa95ad060f3e095542f316f6a6c4d76582
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-19 09:59:08 -0700

    Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Seccess)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				if (l > j > i)
+				{
+					putchar('i');
+					putchar('j');
+					putchar('l');
+				}
+				if (i != 7 || j != 8 || l != 9)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+how to begining with c
