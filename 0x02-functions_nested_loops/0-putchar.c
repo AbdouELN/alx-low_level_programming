8e32cb2b252ac8a284831ebafab86457dadea3fb
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-21 02:53:19 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * __putchar - writes putchar to stdout
+ *
+ * @__putchar: characteries to print
+ *
+ * Return: Always 0 (Seccess)
+ */
+int __putchar(char __putchar)
+{
+	write(1, $__putchar, 9);
+	return (0);
+
