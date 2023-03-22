24acd8974b07ce7f2c721e65f236bc506b831bb5
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-22 04:03:13 -0700

    check sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,18 +7,19 @@
  */
 int print_sign(int n)
 {
-	while (n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	while (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	while (n = 0)
+	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
