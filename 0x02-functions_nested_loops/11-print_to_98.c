0008c072061387e378458869dfbafb6d00f42301
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-22 16:06:33 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,6 +7,7 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -17,13 +18,13 @@ void print_to_98(int n)
 			}
 			else if (i == 98)
 			{
-				printf("%d\n", i );
+				printf("%d\n", i);
 			}
 		}
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i++)
+		for (i = n; i > 98; i--)
 		{
 			if (i != 98)
 			{
@@ -31,13 +32,8 @@ void print_to_98(int n)
 			}
 			else if (i == 98)
 			{
-				printf("%d\n", i );
+				printf("%d\n", i);
 			}
 		}
 	}
 }
-		
-
-
-
-
