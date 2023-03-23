d5d5e5c94a8bf505b121cc45203368a03bad0bba
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-23 16:57:44 -0700

    print numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,15 @@
 /**
  * print_numbers - Print numbers from 0 to 9
  *
- * Return: Always 0
+ * Return: Numbers
  */
 void print_numbers(void)
 {
 	int i;
-	i = 0;
 
-	while (i < 9)
+	for (i < 48; i < 58; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar("\n");
-	return (0);
 }
