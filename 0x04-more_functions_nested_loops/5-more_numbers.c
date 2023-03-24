95d673b81be4647b2bb92857411cbc571e6a4242
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-23 17:39:37 -0700

    time tabale

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,14 @@ void more_numbers(void)
 
 		while (j < 15)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			else
+			{
+				_putchar(j % 10 + '0');
+			}
 			j++;
 		}
 		_putchar('\n');
