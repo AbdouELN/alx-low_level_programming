8e64c36ad99274aa3a78cbf3ac50b76e9c897012
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-23 17:15:02 -0700

    numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@
 void print_most_numbers(void)
 {
 	int i;
-	for (i = 47; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 49 && i != 51)
+		if (i != 50 && i != 52)
 		{
 			_putchar(i);
 		}
