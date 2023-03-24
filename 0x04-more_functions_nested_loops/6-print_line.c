3ec31f05ed814d9d484db9f73f50af5355c153f8
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-24 07:40:21 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,12 +6,18 @@
  */
 void print_line(int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n || i > 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
-		i++;
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
