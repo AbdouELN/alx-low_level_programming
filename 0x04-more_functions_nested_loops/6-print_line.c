36df68ad348203eeffd670678f87f8279147e54b
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-23 17:54:28 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Print line
+ * @n: The number of times the character _ should be printed
+ * Return: Line
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n && i > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
