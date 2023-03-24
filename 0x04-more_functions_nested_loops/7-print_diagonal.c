1e8298f12a9c24c278b124c49de622b5365ec2af
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-24 08:18:05 -0700

    draw with \ line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,17 +14,15 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
