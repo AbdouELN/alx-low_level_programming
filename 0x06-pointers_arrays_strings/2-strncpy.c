2d35cda2e95eeeff56c0c16ede23efaf3b51751e
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-29 15:10:56 -0700

    copiy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncpy - copies a string
+ *
+ * @dest: copiy to
+ *
+ * @src: copiy from
+ *
+ * Return: dest 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		while (j <= n)
+		{
+			dest[i] = src[j];
+		}
+	}
+	dest[i] = '\n';
+	return (dest);
