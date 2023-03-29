f0c3d0bde91ea5b439ea34710b7a341da274d38b
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-29 08:26:24 -0700

    concatenate
    git commit -m concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strcat - that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: dest 
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+		;
+	}
+	if (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	return (dest);
+}
