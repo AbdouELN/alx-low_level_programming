c28e769e000ed5657ab502a45cce882db85e34c4
AbdouELN <aabdoubelkacem@gmail.com>
2023-03-30 12:22:56 -0700

    capitalize strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+ * *cap_string - capitalizes all words of a string.
+ *
+ * @cap: strings input
+ *
+ * Return: cap after capitalizes
+ */
+char *cap_string(char *cap)
+{
+	int i;
+
+	for (i = 0; cap[i] < '\0'; i++)
+	{
+		while (cap[i] = ',' ||
+			cap[i] = ';' ||
+			cap[i] = '.' ||
+			cap[i] = '!' ||
+			cap[i] = '?' ||
+			cap[i] = '(' ||
+			cap[i] = ')' ||
+			cap[i] = '{' ||
+			cap[i] = '}' ||
+			cap[i] = '"' ||
+			cap[i] = ' ' ||
+			cap[i] = '\n' ||
+			cap[i] = '\t' ||
+			i = 0)
+		{
+			cap[i + 1] -= 32;
+		}
+	}
+	return (cap);
+}
