b69ff80dbf518420731e3c5148c32d31c1c34ce5
AbdouELN <aabdoubelkacem@gmail.com>
2023-04-03 19:53:45 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int ir;
 
-	for (ir = 0; n > 0; ir++; n--)
+	for (ir = 0; n > 0; ir++, n--)
 	{
 		s[ir] = b;
 	}
-	return (S);
+	return (s);
 }
