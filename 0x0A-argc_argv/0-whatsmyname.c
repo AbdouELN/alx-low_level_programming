f717eeabe6aa59a98119ffd78ee6efbaf849c2a4
AbdouELN <aabdoubelkacem@gmail.com>
2023-04-10 16:52:01 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print name of program
+ * @argc: integer
+ * @argv: list
+ * Return: always 0
+ */
+void main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
