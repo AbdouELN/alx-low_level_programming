7320a04c6e32c230bab6527fa798f8afc25adac4
AbdouELN <aabdoubelkacem@gmail.com>
2023-04-10 18:05:02 -0700

    sum of two positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: arguments counter
+ * @argv: arguments value
+ * return: 1 if one of the numbers contain not digits symbols otherwise 0(Success)
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+		{
+			if (*c < 0 || *c > 9)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		res += atoi(argv[argc]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
+
