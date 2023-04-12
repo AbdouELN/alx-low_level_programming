43977dee1bbae0b0a633b15cc2c68fe8a0e5a08a
AbdouELN <aabdoubelkacem@gmail.com>
2023-04-11 18:02:50 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,17 +10,16 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i;
-	int j;
 	int lens1, lens2;
 	char *s3;
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = '\0';
 	}
 	for (lens1 = 0; s1[lens1] != '\0'; lens1++)
 	{
@@ -34,17 +33,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (s3 == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
-	while (s1[i] != '\0')
+	for (i = 0; i <= lens1 + lens2; i++)
 	{
-		s3[i] = s1[i];
-		i++;
-		while (s2[j] != '\0')
+		if (i < lens1)
 		{
-			s3[i] = s2[j];
-			i++;
-			j++;
+			s3[i] = s1[i];
+		}
+		else
+		{
+			s3[i] = s2[i - lens1];
 		}
 	}
 	s3[i] = '\0';
