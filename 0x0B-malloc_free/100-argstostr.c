45f83764c13e2e11f02ba0e5eb86ddb863773bc4
AbdouELN <aabdoubelkacem@gmail.com>
2023-04-12 18:01:09 -0700

    argostostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,7 +27,7 @@ char *argstostr(int ac, char **av)
 	}
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++, l++)
 		{
 			str[l] = av[i][j];
 		}
