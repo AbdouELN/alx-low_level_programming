b16996b82b45174ff6122acdc2d2c2b699b077ad
AbdouELN <aabdoubelkacem@gmail.com>
2023-05-02 13:10:55 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
-section        .texte
+section        .text
         extern printf
         global main
 main:
-        mv            edi, messag
+        mv            edi, msg
         mv            eax, 0
         call   printf
 
 section        .data
-        messag db 'Hello, Holberton', 0xa, 0
+        msg    db 'Hello, Holberton', 0xa, 0
