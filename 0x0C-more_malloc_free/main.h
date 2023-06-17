e0094d7288d13126a55a3c1d8d6bedd1f49cd221
AbdouELN <aabdoubelkacem@gmail.com>
2023-06-17 04:32:38 -0700

    checked

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *str, char by, unsigned int n);
+
+#endif
